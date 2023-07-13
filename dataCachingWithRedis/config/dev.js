9b4c3c029093f16ed2cfbc284e135105b3621659
arpitktyr <arpitktyr@gmail.com>
2023-07-13 14:08:18 +0530

    project setup completed

---
+++ dataCachingWithRedis/config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://arpitktyr:K5oBYW4cs4NnsKpm@cluster0.iiu1pbu.mongodb.net/NodeJSPerformanceDemo?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+};
